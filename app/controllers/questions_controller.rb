89c4467c95a75d22c47d9dddb113f16717191ab2
Matteo Sansalvadore <matteosansalvadore@gmail.com>
2022-11-23 14:53:36 +0100

    f

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,4 +4,19 @@ class QuestionsController < ApplicationController
   #   # verb "path", to: "controller#action"
   #   get "about", to: "pages#about"
   # end
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+      if @question.blank?
+        @answer = "I can't hear you!"
+      elsif @question =~ /i going to work i/
+        @answer = "great!"
+      elsif @question.ends_with?("?")
+        @answer = "no questions"
+      else
+        @answer = "dont care"
+      end
+  end
 end
