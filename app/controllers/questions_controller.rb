0d539745051baecba8cd6f586d1c06e0b9ae3d91
Matteo Sansalvadore <matteosansalvadore@gmail.com>
2022-11-23 11:19:48 +0100

    rails new

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,7 @@
+class QuestionsController < ApplicationController
+  # Rails.application.routes.draw do
+  #   # Generic syntax:
+  #   # verb "path", to: "controller#action"
+  #   get "about", to: "pages#about"
+  # end
+end
